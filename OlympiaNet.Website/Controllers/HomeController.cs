c4bb9ca3393f7d8fe0a6b97e79bc2a5ccd4eab19
Tien Nguyen <lilylnx@gmail.com>
2012-02-23 21:10:33 +0700

    Important changes! Using Castle.Windsor IoC instead.
    Signed-off-by: Tien Nguyen <lilylnx@gmail.com>

--- OlympiaNet.Website/Controllers/HomeController.cs
+++ OlympiaNet.Website/Controllers/HomeController.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
 
+using Castle.Core.Logging;
+
 namespace OlympiaNet.Website.Controllers
 {
     [HandleError]
     public class HomeController : Controller
     {
+        public ILogger Logger { get; set; }
+
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
-            ViewBag.abc = "Xin Chao!";
+            ViewBag.Welcome = "Olympia!";
+            Logger.WarnFormat("OK!");
             return View();
         }
     }
---
+++ OlympiaNet.Website/Main/Bootstrap.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+
+using SaberLily.Web.Factory;
+
+namespace OlympiaNet.Website.Main
+{
+    /// <summary>
+    /// Lớp khởi tạo WindsorContainer, dùng làm nền tảng để kết hợp những module khác.
+    /// </summary>
+    public sealed class Bootstrap
+    {
+        private static IWindsorContainer container;
+
+        public static Bootstrap Instance
+        {
+            get { return BootstrapHolder.Instance; }
+        }
+
+        /// <summary>
+        /// Hàm dựng private ngăn cản việc khởi tạo từ class khác.
+        /// </summary>
+        private Bootstrap() { }
+
+        /// <summary>
+        /// Khởi tạo các thành phần cơ bản cho web.
+        /// </summary>
+        public void Init()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            InitContainer();
+            InitControllerFactory();
+        }
+
+        /// <summary>
+        /// Hủy và thu hồi tài nguyên web.
+        /// </summary>
+        public void Dispose()
+        {
+            container.Dispose();
+        }
+
+        private void InitContainer()
+        {
+            container = new WindsorContainer().Install(FromAssembly.This());
+        }
+
+        private void InitControllerFactory()
+        {
+            var controllerFactory = new WindsorControllerFactory(container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+
+        private void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        private void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                } // Parameter defaults
+            );
+        }
+
+        #region ----- BootstrapHolder Class
+
+        /// <summary>
+        /// Lớp BootstrapHolder ở dạng private là một giải pháp thread-safe mà không cần
+        /// một cấu trúc đặc biệt khác tương tự để thay thế.
+        /// </summary>
+        private static class BootstrapHolder
+        {
+            public static readonly Bootstrap Instance = new Bootstrap();
+        }
+
+        #endregion
+    }
+}
