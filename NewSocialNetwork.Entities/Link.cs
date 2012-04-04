3c90a98f2336cf604d6fa876c5636d33b844dae6
Lien Pham <insaber2309@yahoo.com>
2012-04-04 15:57:04 +0700

    Added some new entities.
    Signed-off-by: Lien Pham <insaber2309@yahoo.com>

---
+++ NewSocialNetwork.Entities/Link.cs
@@ -0,0 +1,41 @@
+﻿using Castle.ActiveRecord;
+
+namespace NewSocialNetwork.Entities
+{
+    [ActiveRecord("[NSN.Link]", "dbo", Lazy = true)]
+    public class Link : ActiveRecordBase<Link>
+    {
+        [PrimaryKey(PrimaryKeyType.Identity, "LinkId")]
+        public virtual int LinkId { get; set; }
+
+        public virtual int UserId { get; set; }
+
+        public virtual int FriendUserId { get; set; }
+
+        [Property("Privacy", NotNull = true, Default = "0")]
+        public virtual byte Privacy { get; set; }
+
+        [Property("Link", Length = 255, NotNull = true)]
+        public virtual string Link { get; set; }
+
+        [Property("Image", Length = 255, NotNull = false)]
+        public virtual string Image { get; set; }
+
+        [Property("Title", Length = 255, NotNull = false)]
+        public virtual string Title { get; set; }
+
+        [Property("Description", Length = 255, NotNull = false)]
+        public virtual string Description { get; set; }
+
+        [Property("Timestamp", NotNull = true)]
+        public virtual int Timestamp { get; set; }
+
+        [Property("TotalComment", NotNull = true, Default = "0")]
+        public virtual int TotalComment { get; set; }
+
+        [Property("TotalLike", NotNull = true, Default = "0")]
+        public virtual int TotalLike { get; set; }
+
+        public Link() { }
+    }
+}
---
+++ NewSocialNetwork.Entities/PhotoAlbum.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+using Castle.ActiveRecord;
+
+namespace NewSocialNetwork.Entities
+{
+    [ActiveRecord("NSN.PhotoAlbum", "dbo", Lazy = true)]
+    public class PhotoAlbum : ActiveRecordBase<PhotoAlbum>
+    {
+        [PrimaryKey(PrimaryKeyType.Identity, "AlbumId")]
+        public virtual int AlbumId { get; set; }
+
+        public virtual int UserId { get; set; }
+
+        public virtual int ProfileId { get; set; }
+
+        [Property("Privacy", NotNull = true)]
+        public virtual int Privacy { get; set; }
+
+        [Property("PrivacyComment", NotNull = true)]
+        public virtual int PrivacyComment { get; set; }
+
+        [Property("Name", Length = 255, NotNull = true)]
+        public virtual string Name { get; set; }
+
+        [Property("Timestamp", NotNull = true)]
+        public virtual int Timestamp { get; set; }
+
+        [Property("TotalPhoto", NotNull = true)]
+        public virtual int TotalPhoto { get; set; }
+
+        [Property("TotalComment", NotNull = true)]
+        public virtual int TotalComment { get; set; }
+
+        [Property("TotalLike", NotNull = true)]
+        public virtual int TotalLike { get; set; }
+
+        public PhotoAlbum() { }
+    }
+}
