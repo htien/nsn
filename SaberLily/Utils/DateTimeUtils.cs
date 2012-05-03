02639e4a2dff6911d5c1826663ce9a9c72bf649c
Tien Nguyen <lilylnx@gmail.com>
2012-05-03 13:31:48 +0700

    Modified DateTimeUtils.cs
    Signed-off-by: Tien Nguyen <lilylnx@gmail.com>

--- SaberLily/Utils/DateTimeUtils.cs
+++ SaberLily/Utils/DateTimeUtils.cs
@@ -4,9 +4,19 @@ namespace SaberLily.Utils
 {
     public static class DateTimeUtils
     {
+        public static DateTime Jan1st1970 
+        {
+            get { return DateTime.Parse("1970/01/01 00:00:00"); }
+        }
+
+        public static long CurrentTimeMillis
+        {
+            get { return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds; }
+        }
+
         public static long UnixTicks
         {
-            get { return DateTime.UtcNow.Ticks - DateTime.Parse("1970/01/01 00:00:00").Ticks; }
+            get { return DateTime.UtcNow.Ticks - Jan1st1970.Ticks; }
         }
 
         public static int UnixTimestamp
